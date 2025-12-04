4d133319ff0f92e6c2128016a0129614e0c5a99a
Ritesh Paryali <ritesh.paryali@in.ey.com>
2025-12-04 14:04:19 +0530

    dynamo db client created

---
+++ src/config/dynamodb.js
@@ -0,0 +1,14 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamodbDocumentClient } = require("@aws-sdk/lib-dynamodb");
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION || "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const docClient = DynamodbDocumentClient.from(client);
+
+module.exports = { docClient, client };
