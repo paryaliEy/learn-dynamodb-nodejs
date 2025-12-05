e66030bef653262748d3ae661d1eb5d9c1f26c5d
Ritesh Paryali <ritesh.paryali@in.ey.com>
2025-12-05 10:55:24 +0530

    credentials not required for serverless

--- src/config/dynamodb.js
+++ src/config/dynamodb.js
@@ -3,10 +3,10 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 export const client = new DynamoDBClient({
   region: process.env.AWS_REGION || "us-east-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+  // credentials: {
+  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  // },
 });
 
 export const docClient = DynamoDBDocumentClient.from(client, {
